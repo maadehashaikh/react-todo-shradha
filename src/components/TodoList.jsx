55ca117a9c2ecf37a570b3fc4ad4834388454a9f
Maadeha shaikh <maadeham@gmail.com>
2024-04-28 20:56:59 +0500

    second commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
-
 function TodoList() {
   let [todos , setTodos] = useState([{task:"Sample task" , id:uuidv4()}]);
   let [newTodo,setNewtodo] = useState("");
-
-  let addNewTask = () => {
-  setTodos([...todos , {task:newTodo , id:uuidv4()}]);
+  let addNewTask = () => { 
+  setTodos((prevTodos)=> {
+    return [...prevTodos , {task : newTodo , id:uuidv4()}]
+  });
   setNewtodo("");
   } 
   let updateTodoItem = (event) => {
       setNewtodo(event.target.value);
   }
+  const deleteTodo = (id) => {
+    setNewtodo((prevTodos) => {
+      todos.filter((prevTodos) => prevTodos.id != id)
+  });
+  };
   return (
     <>
     <input type="text" placeholder='Add your todo item'value={newTodo}  onChange={updateTodoItem}/>
@@ -21,11 +26,14 @@ function TodoList() {
     <ul>
       {
         todos.map((todo) => (
-          <li key={todo.id}>{todo.task}</li>
+          
+          <li key={todo.id}> <span>{todo.task}</span> 
+          &nbsp;
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
         ))}
     </ul>
     </>
   )
 }
-
 export default TodoList
