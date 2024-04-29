aebbdb5a439ed5bcd0af821009230ec8ad765fec
Maadeha shaikh <maadeham@gmail.com>
2024-04-29 11:34:54 +0500

    3rd commmit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -14,10 +14,35 @@ function TodoList() {
       setNewtodo(event.target.value);
   }
   const deleteTodo = (id) => {
-    setNewtodo((prevTodos) => {
-      todos.filter((prevTodos) => prevTodos.id != id)
-  });
+    setNewtodo((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
   };
+
+  let upperCase = () => {
+  setTodos((todos) => 
+    todos.map((todo)=> {
+    return {
+      ...todo,
+      task:todo.task.toUpperCase(),
+    };
+  })
+);
+}
+
+const upperCaseOne = (id)  => {
+  setTodos((prevTodos) => 
+  prevTodos.map((todo)=> {
+    if(todo.id == id){
+  return {
+    ...todo,
+    task:todo.task.toUpperCase(),
+  };
+}
+else{
+  return todo;
+}
+})
+);
+}
   return (
     <>
     <input type="text" placeholder='Add your todo item'value={newTodo}  onChange={updateTodoItem}/>
@@ -30,9 +55,12 @@ function TodoList() {
           <li key={todo.id}> <span>{todo.task}</span> 
           &nbsp;
           <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          <button onClick={() => upperCaseOne(todo.id)}>upperCaseOne</button>
           </li>
         ))}
     </ul>
+    <br></br>
+    <button onClick={upperCase}>UpperCaseAll</button>
     </>
   )
 }
